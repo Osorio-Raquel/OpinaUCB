a7c39b4c12e8e890aeceab98d7379983812d69c9
Sergio_Arias <148729515+SaiPlusSai@users.noreply.github.com>
2025-09-08 00:41:40 -0400

    prototipo1
    prototipo1

--- proto_kpis_t1/test/widget_test.dart
+++ proto_kpis_t1/test/widget_test.dart
@@ -11,13 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:proto_kpis_t1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('La app arranca y muestra las tabs', (tester) async {
+    await tester.pumpWidget(const ProtoApp());
+    await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Formulario'), findsOneWidget);
+    expect(find.text('Dashboard'), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
